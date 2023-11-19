4e5136c247e2c914a2380a923558d476f9d125b7
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-11-19 23:39:45 +0100

    --

---
+++ tp6/src/main/java/com/example/tp6/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.tp6;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ tp6/src/main/java/entities/Compt.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.util.Date;
+
+public class Compt {
+private  static long   id ;
+private double old ;
+private  Date dateCreation;
+private  TypeCompte type ;
+private  String etat ;
+
+}
